df301381281a053a230e84a915ea34186796d5d8
Md Shihab <121821689+IamShihabPro@users.noreply.github.com>
2023-01-03 00:52:10 +0600

    Add files via upload

---
+++ A.py
@@ -0,0 +1,16 @@
+# v=['es','ES','eS','Es']
+# s= input()
+# k=len(s)
+# for i in range (int(input())):
+#     for i in (k):
+#         if v in s:
+#             print("YES")
+#         else:
+#             print("No")
+
+n = int(input())
+l ='es'
+for i in range (n):
+    s= str(input())
+    for l in s:
+        
